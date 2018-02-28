a064a46333f293d8408a6ce694763bbd123bd6ea
Dan Leehr <dan.leehr@duke.edu>
2018-02-28 15:35:07 -0500

    Implements real deletion with --delete option
    Otherwise dry run

--- pruner.py
+++ pruner.py
@@ -4,6 +4,7 @@ import os
 import time
 import logging
 import sys
+import shutil
 
 logging.basicConfig(level=logging.INFO)
 
@@ -32,7 +33,7 @@ def get_age_days(path):
   now = time.time()
   age_seconds = now - mtime
   age_days = age_seconds // 86400
-  return age_days
+  return int(age_days)
 
 def get_oldest_directory(parent, exclude=[]):
   children = os.listdir(parent)
@@ -61,6 +62,7 @@ def get_free_bytes(path):
 
 def delete(path):
   logging.debug('Deleting {0}'.format(path))
+  shutil.rmtree(path) # ignore errors
 
 def format_size(bytes):
   if bytes > TB: divisor, unit = TB, 'TB'
@@ -69,14 +71,15 @@ def format_size(bytes):
   else: divisor, unit = KB, 'KB'
   return '{0} {1}'.format(bytes / divisor, unit)
 
-def clean(path, desired_free_bytes, min_age_days):
-  logging.info('Starting, targeting {0} free and removing directories older than {1} days'.format(format_size(desired_free_bytes), min_age_days))
+def clean(path, desired_free_bytes, min_age_days, dry_run=True):
+  free_bytes = get_free_bytes(path)
+  logging.info('Starting, current free space: {0}, desired {1}. Considering directories older than {2} days'.format(format_size(free_bytes), format_size(desired_free_bytes), min_age_days))
 
   # For dry-run
   reclaimed_dirs=[]
   reclaimed_bytes=0
+  prefix = 'DRY-RUN:' if dry_run else ''
 
-  free_bytes = get_free_bytes(path)
   while free_bytes < desired_free_bytes:
     logging.debug('Current free space: {0}, desired: {1}'.format(format_size(free_bytes), format_size(desired_free_bytes)))
     oldest_directory = get_oldest_directory(path, exclude=reclaimed_dirs)
@@ -87,10 +90,13 @@ def clean(path, desired_free_bytes, min_age_days):
     absolute_path = os.path.join(path, name)
     if age_days > min_age_days:
       size = get_size(absolute_path)
-      logging.info('Deleting oldest directory {0} ({1} days) to reclaim {2}'.format(name, age_days, format_size(size)))
-      delete(name)
-      reclaimed_bytes += size
-      reclaimed_dirs.append(name)
+      logging.info('{0}Removing {1}. Age: {2} days, Size: {3}'.format(prefix, name, age_days, format_size(size)))
+      if dry_run:
+        # on dry_run, won't actually delete anything, but do count
+        reclaimed_bytes += size
+        reclaimed_dirs.append(name)
+      else:
+        delete(absolute_path)
     else:
       logging.info('Stopping: Oldest directory {0} age is {1} days, newer than our minimum of {2}'.format(name, age_days, min_age_days))
       break
@@ -100,12 +106,19 @@ def clean(path, desired_free_bytes, min_age_days):
 
 def main():
   if len(sys.argv) < 4:
-    print 'Usage: {0} <path> <desired free space in TB> <number of days to keep>'.format(sys.argv[0])
+    print 'Usage: {0} <path> <desired free space in TB> <number of days to keep> [--delete]\n'.format(sys.argv[0])
+    print 'Without --delete, this script will only report what would be deleted to meet the desired metrics'
     exit(1)
   path = sys.argv[1]
   desired_free_bytes = float(sys.argv[2]) * TB
   min_age_days = float(sys.argv[3])
-  clean(path, desired_free_bytes, min_age_days)
+
+  if '--delete' in sys.argv:
+    dry_run = False
+  else:
+    dry_run = True
+
+  clean(path, desired_free_bytes, min_age_days, dry_run)
 
 if __name__ == '__main__':
   main()
